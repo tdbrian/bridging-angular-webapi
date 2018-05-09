15bbbef88c05930b0fba4a7c4c9c977a0b2b617f
Thomas Brian <tbrian@appriver.com>
2018-05-09 11:34:45 -0500

    Add get command in app.component

--- TourOfHeroesClient/src/app/app.component.ts
+++ TourOfHeroesClient/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from './api/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private heroesService: HeroesService) {}
+
+  async ngOnInit(): Promise<void> {
+    const heroes = await this.heroesService.ApiV1HeroesGet().toPromise();
+    console.info(heroes);
+  }
 }
